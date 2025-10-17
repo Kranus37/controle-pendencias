259750126620657988249a800bfe182e9c732b60
Kranus37 <samuel.resende6637@gmail.com>
2025-10-17 05:34:46 -0300

    Update DBConnection.java

--- src/main/java/com/example/controlependencias/util/DBConnection.java
+++ src/main/java/com/example/controlependencias/util/DBConnection.java
@@ -6,12 +6,12 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/controle_pendencias";
-    private static final String USER = "root"; // Altere para o seu usuário do MySQL
-    private static final String PASSWORD = ""; // Altere para a sua senha do MySQL
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "admin123"; 
 
     public static Connection getConnection() throws SQLException {
         try {
-            // Carrega o driver JDBC do MySQL
+            
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
